4b661de52bb271a32722792321dc6e3f1b7da7a1
doctor_zalman <georgesun30@gmail.com>
2022-05-14 13:11:48 +0300

    testong components

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,12 +1,8 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-// import {API_KEY} from "../consts";
 
 const instance: AxiosInstance = axios.create({
   baseURL: 'https://api.exchangerate.host/',
   responseType: 'json',
-  // headers: {
-  //   "apikey": API_KEY
-  // }
 });
 
 const get = <R>(url: string, config?: AxiosRequestConfig): Promise<R> =>
